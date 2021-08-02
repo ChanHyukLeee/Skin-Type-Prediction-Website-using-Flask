a5a654d034985c3c9ba797ad6c33373abf286cd8
JustCal1MeLee <kkmi1127@gmail.com>
2021-08-02 12:43:43 +0900

    revise some code

--- api.py
+++ api.py
@@ -2,21 +2,34 @@ import os
 from flask import Flask
 from flask import request
 from flask import render_template
+from flask import jsonify
+from torch_utils import transform_image, prediction
+
+UPLOAD_FOLDER = 'D:\learn_flask\Practice_Flask\static'
+ALLOWED_FILE = ['jpg', 'png', 'jpeg' ]
 
 app = Flask(__name__)
-UPLOAD_FOLDER = "D:\learn_flask\Practice_Flask\static"
 
 @app.route("/", methods = ["GET", "POST"])
 def upload_predict():
     if request.method == "POST":
         image_file = request.files["image"]
+        if image_file is None or image_file.filename =="":
+            return jsonify({'error': 'no file'})
+        if not ALLOWED_FILE(image_file.filename):
+            return jsonify({'error': 'format not supported'})
         if image_file:
             image_location = os.path.join(
                 UPLOAD_FOLDER, image_file.filename
             )
             image_file.save(image_location)
-            return render_template("index.html", prediction=1, image_loc = image_file.filename)
-    return render_template("index.html", prediction=0, image_file = None)
+            img_bytes = image_file.read()
+            img_tensor = transform_image(img_bytes)
+            pred = prediction(img_tensor)
+            
+            return render_template("index.html", prediction=pred, image_loc = image_file.filename)
+    pred = ""
+    return render_template("index.html", prediction=pred, image_file = None)
 
 if __name__ == "__main__":
     app.run(port=1200, debug=True)
