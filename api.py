987bf67bfb10d99c2206a762edb7fdc075741fda
JustCal1MeLee <kkmi1127@gmail.com>
2021-08-05 11:31:15 +0900

    load model into flask

--- api.py
+++ api.py
@@ -4,9 +4,16 @@ from flask import request
 from flask import render_template
 from flask import jsonify
 from torch_utils import transform_image, prediction
+import torch
 
 UPLOAD_FOLDER = 'D:\learn_flask\Practice_Flask\static'
 ALLOWED_FILE = ['jpg', 'png', 'jpeg' ]
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+MODEL_PATH ='D:\learn_flask\Practice_Flask\model.pth'
+model = None
+model= torch.load(MODEL_PATH, map_location = DEVICE)
+model.eval()
 
 app = Flask(__name__)
 
@@ -14,18 +21,18 @@ app = Flask(__name__)
 def upload_predict():
     if request.method == "POST":
         image_file = request.files["image"]
-        if image_file is None or image_file.filename =="":
-            return jsonify({'error': 'no file'})
-        if not ALLOWED_FILE(image_file.filename):
-            return jsonify({'error': 'format not supported'})
+        # if image_file is None or image_file.filename =="":
+        #     return jsonify({'error': 'no file'})
+        # if not ALLOWED_FILE(image_file.filename):
+        #     return jsonify({'error': 'format not supported'})
         if image_file:
             image_location = os.path.join(
                 UPLOAD_FOLDER, image_file.filename
             )
             image_file.save(image_location)
-            img_bytes = image_file.read()
-            img_tensor = transform_image(img_bytes)
-            pred = prediction(img_tensor)
+            # img_bytes = image_file.read()
+            img_tensor = transform_image(image_location)
+            pred = prediction(img_tensor = img_tensor, model=model)
             
             return render_template("index.html", prediction=pred, image_loc = image_file.filename)
     pred = ""
--- torch_utils.py
+++ torch_utils.py
@@ -1,6 +1,5 @@
 import torch
 import torchvision.transforms as transforms
-import torch.nn as nn
 from PIL import Image
 import io
 
@@ -9,38 +8,38 @@ DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 # if DEVICE == 'cuda':
 #     torch.cuda.manual_seed_all(777)
 
-MODEL_PATH ='D:\learn_flask\Practice_Flask\model.pth'
+
 LABELS = [
     'combination',
     'dry',
     'oily',
     'sensitive'
 ]
-model = None
-model= torch.load(MODEL_PATH)
-model.to(DEVICE)
-model.eval()
+
+
 
 mean=[0.485, 0.456, 0.406]
 std=[0.229, 0.224, 0.225]
 
+transform = transforms.Compose([
+                        # transforms.Grayscale(num_output_channels=1),
+                        transforms.Resize([300,300]),
+                        transforms.ToTensor(),
+                        # transforms.RandomHorizontalFlip(p=0.5),
+                        # transforms.RandomVerticalFlip(p=0.5),  
+                        transforms.Normalize(torch.Tensor(mean),torch.Tensor(std))
+                        ])
+
 # albumentation으로 변경 고려
-def transform_image(image_bytes):
-    transform = transforms.Compose([
-                            # transforms.Grayscale(num_output_channels=1),
-                            transforms.Resize([300,300]),
-                            transforms.ToTensor(),
-                            # transforms.RandomHorizontalFlip(p=0.5),
-                            # transforms.RandomVerticalFlip(p=0.5),  
-                            transforms.Normalize(torch.Tensor(mean),torch.Tensor(std))
-                            ])
-    image = Image.open(io.BytesIO(image_bytes))
-    image = transform(image).float()
-    return image.unsqueeze(0)
+def transform_image(image_location):
+    # image = Image.open(io.BytesIO(image_bytes))
+    image = Image.open(image_location)
+    image = transform(image).unsqueeze(0).to(DEVICE)
+    return image
 
 def prediction(img_tensor, model):
-    with torch.no_grad:
-        output = model(img_tensor)
-        _, predicted = torch.max(output.data, 1)
+    # with torch.no_grad:
+    output = model(img_tensor)
+    _, predicted = torch.max(output.data, 1)
     return LABELS[predicted.item()]
 
